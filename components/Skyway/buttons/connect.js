23513bbe98b7013774eb25a2412d404fcfb03277
sasayabaku <sasayabaku@yahoo.co.jp>
2021-10-09 16:30:13 +0900

    Skyway > create > 切断と再接続処理

---
+++ components/Skyway/buttons/connect.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Button from'@material-ui/core/Button';
+
+const ConnectButton = () => {
+    return (
+        <>
+            <Button variant="outlined">切断</Button>
+        </>
+    );
+};
+
+export default ConnectButton;
